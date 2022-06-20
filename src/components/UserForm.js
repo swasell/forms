8b676eefd6d27e4aa257e6b445cf98775663889f
swasell <svenwasell@gmail.com>
2022-06-19 17:34:10 -0700

    Updated for More Forms

--- src/components/UserForm.js
+++ src/components/UserForm.js
@@ -7,48 +7,36 @@ const UserForm = (props) => {
     const [password, setPassword] = useState("");
     const [confirmPassword, setConfirmPassword] = useState("");
 
-    const createUser = (e) => {
-
-        e.preventDefault();
-
-        const newUser = { firstName, lastName, email, password, confirmPassword };
-        setFirstName("");
-        setLastName("");
-        setEmail("");
-        setPassword("");
-        setConfirmPassword("");
-    };
-
     return(
-        <div>
-            <form onSubmit={ createUser }>
-                <div>
-                    <label> First Name: </label>
-                    <input type="text" value={firstName} onChange= { (e) => 
-                    setFirstName(e.target.value) } />
-                </div>
-                <div>
-                    <label> Last Name: </label>
-                    <input type="text" value={lastName} onChange= { (e) => 
-                    setLastName(e.target.value) } />
-                </div>
-                <div>
-                    <label> Email: </label>
-                    <input type="text" value={email} onChange= { (e) => 
-                    setEmail(e.target.value) } />
-                </div>
-                <div>
-                    <label> Password: </label>
-                    <input type="text" value={password} onChange= { (e) => 
-                    setPassword(e.target.value) } />
-                </div>
-                <div>
-                    <label> Confirm Password: </label>
-                    <input type="text" value={confirmPassword} onChange= { (e) => 
-                    setConfirmPassword(e.target.value) } />
-                </div>
-                <input type="submit" value="Create User" />
-            </form>
+        <>
+        <form onSubmit={ () => {}}>
+            <div>
+                <label> First Name: </label>
+                <input type="text" onChange= { (e) => setFirstName(e.target.value) } />
+                { firstName.length < 2 ? (<p>First name must be at least 2 characters</p>) : null }
+            </div>
+            <div>
+                <label> Last Name: </label>
+                <input type="text" onChange= { (e) => setLastName(e.target.value) } />
+                { lastName.length < 2 ? (<p>Last name must be at least 2 characters</p>) : null }
+            </div>
+            <div>
+                <label> Email: </label>
+                <input type="text" onChange= { (e) => setEmail(e.target.value) } />
+                { email.length < 5 ? (<p>Email must be at least 5 characters</p>) : null }
+            </div>
+            <div>
+                <label> Password: </label>
+                <input type="password" onChange= { (e) => setPassword(e.target.value) } />
+                { password.length < 8 ? (<p>Password must be at least 8 characters</p>) : null }
+            </div>
+            <div>
+                <label> Confirm Password: </label>
+                <input type="password" onChange= { (e) => setConfirmPassword(e.target.value) } />
+                { confirmPassword !== password ? (<p>Passwords must match</p>) : null }
+            </div>
+            <input type="submit" value="Create User" />
+        </form>
             <div>
                 <h2>
                     Your Form Data
@@ -69,10 +57,10 @@ const UserForm = (props) => {
                     Confirm Password: { confirmPassword }
                 </p>
             </div>
-        </div>
+        </>
         
-    )
+    );
+};
 
-}
 
 export default UserForm;
